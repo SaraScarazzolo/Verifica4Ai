f0eba9cc19454e464ad1ed5ec98add0f59f7718f
luca glessi <luca.glessi@isitgo.it>
2023-05-19 10:11:38 +0200

    Alfonso
    Alfonso

---
+++ Assets/collisione.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collisione : MonoBehaviour
+{
+    private void OnTriggerEnter2d(Collider2d collision)
+    {
+    print("sei scarso");
+        this.gameObject.SetActive(false);
+    
+
+    }
+
+}
